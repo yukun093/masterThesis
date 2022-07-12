74d591eda32c56e38e5c0fa4008403c7acd2f2cf
yukun093 <yukun093@163.com>
2022-07-12 17:48:26 +0300

    update

---
+++ repo/kitti_native_evaluation/CMakeFiles/evaluate_object_offline.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yukun/Documents/Master_Thesis/masterThesis/repo/Point-GNN/kitti_native_evaluation/src/evaluate_object_3d_offline.cpp" "/home/yukun/Documents/Master_Thesis/masterThesis/repo/Point-GNN/kitti_native_evaluation/CMakeFiles/evaluate_object_offline.dir/src/evaluate_object_3d_offline.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ repo/kitti_native_evaluation/CMakeFiles/evaluate_object_offline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/evaluate_object_offline.dir/src/evaluate_object_3d_offline.cpp.o"
+  "evaluate_object_offline"
+  "evaluate_object_offline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/evaluate_object_offline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
